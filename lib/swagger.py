17a046f43bbe374b086b132eb010091f99a1d371
Todd Treece <toddtreece@gmail.com>
2016-10-20 17:06:05 -0400

    add debug helper and mitm start script

--- lib/swagger.py
+++ lib/swagger.py
@@ -11,33 +11,51 @@ class Swagger:
         'paths': {},
         'schemes': []
       }
+
     return self.hosts[req.host]
 
   def get_path(self, req):
      host = self.get_host(req)
 
-     if req.path not in host['paths']:
-       host['paths'][req.path] = {}
+     if '?' in req.path:
+       path = req.path[:req.path.find('?')]
+     else:
+       path = req.path
 
-     return host['paths'][req.path]
+     if path not in host['paths']:
+       host['paths'][path] = {}
+
+     return host['paths'][path]
 
   def get_method(self, req):
      method = req.method.lower()
      path = self.get_path(req)
+
      if method not in path:
        path[method] = {
          'consumes': [],
          'parameters': [],
          'responses': {}
        }
+
      return path[method]
 
   def parse_request(self, req):
     self.get_method(req)
 
+  def print_debug(self):
+    for host in self.hosts:
+      print(self.hosts[host]['host'])
+
+      for path in self.hosts[host]['paths']:
+        print('  - {0}'.format(path))
+
+    print('--------------------------------')
+
   def request(self, flow):
     self.parse_request(flow.request)
-    print(self.hosts)
+    self.print_debug()
+
 
 def start():
     return Swagger()
