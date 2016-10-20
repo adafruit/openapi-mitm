bc35d3a62b398ccf1f1d23c5029840e3c05e6025
Todd Treece <toddtreece@gmail.com>
2016-10-20 16:26:49 -0400

    start of parsing request

--- swagger.py
+++ swagger.py
@@ -1,3 +1,29 @@
-def request(flow):
-  for header in flow.request.headers:
-    print(header)
+class Swagger:
+
+  def __init__(self):
+    self.hosts = {}
+
+  def add_host(self, req):
+    if req.host not in self.hosts:
+      self.hosts[req.host] = {}
+
+  def add_path(self, req):
+     if req.path not in self.hosts[req.host]:
+       self.hosts[req.host][req.path] = {}
+
+  def add_method(self, req):
+     method = req.method.lower()
+     if method not in self.hosts[req.host][req.path]:
+       self.hosts[req.host][req.path][method] = {}
+
+  def parse_request(self, req):
+    self.add_host(req)
+    self.add_path(req)
+    self.add_method(req)
+
+  def request(self, flow):
+    self.parse_request(flow.request)
+    print(self.hosts)
+
+def start():
+    return Swagger()
